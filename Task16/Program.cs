f4030a2112a6e0041447523cc32551bc341cf61f
OlesyaFedorovskaya <nyamka13@mail.ru>
2022-11-21 00:56:12 +0300

    added Tasks 16

--- Task16/Program.cs
+++ Task16/Program.cs
@@ -5,3 +5,17 @@
 // -4, 16 -> да
 // 25, 5 -> да
 // 8,9 -> нет
+
+Console.WriteLine("Введите первое число:");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Является ли одно число квадратом другого: ");
+
+Console.WriteLine(square(num1, num2) ? "Да" : "Нет");
+
+bool square(int firstNum, int secondNum)
+{
+    return (firstNum == secondNum * secondNum) || (secondNum == firstNum * firstNum);
+}
