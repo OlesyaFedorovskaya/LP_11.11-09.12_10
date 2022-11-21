be80aeb5d20477e36b59afecab9503122bc91e64
OlesyaFedorovskaya <nyamka13@mail.ru>
2022-11-21 05:44:09 +0300

    added Tasks 15

--- Task15/Program.cs
+++ Task15/Program.cs
@@ -4,3 +4,19 @@
 // 6 -> да
 // 7 -> да
 // 1 -> нет
+
+Console.Write("Введите цифру обозначающую день недели: ");
+int day = Convert.ToInt32(Console.ReadLine());
+
+if (day == 6 || day == 7)
+{
+    Console.WriteLine("Выходной день");
+}
+else if (day < 1 || day > 7)
+{
+    Console.WriteLine("Вы ввели несуществующий день недели");
+}
+else
+{
+    Console.WriteLine("Будний день");
+}
