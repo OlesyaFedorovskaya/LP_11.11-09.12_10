32c1f1d7b65cd5f955a36ab79f97c1aa879df786
OlesyaFedorovskaya <nyamka13@mail.ru>
2022-11-21 06:24:54 +0300

    added Tasks 13

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -4,3 +4,29 @@
 // 645 -> 5
 // 78 -> третьей цифры нет
 // 32679 -> 6
+
+Console.Write("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int DigitFind(int number)
+{
+    int result = -1;
+    if (number >= 100)
+    {
+        while (number > 999)
+        {
+            number = number / 10;
+        }
+        result = number % 10;
+    }
+    return result;
+}
+
+if (DigitFind(number) == -1)
+{
+    Console.WriteLine("Третьей цифры не существует");
+}
+else
+{
+    Console.WriteLine($"Третья цифра: {DigitFind(number)}");
+}
