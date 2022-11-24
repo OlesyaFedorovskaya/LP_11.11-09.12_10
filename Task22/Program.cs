eea884e862f37725bd7d65bb281b24535dcd34c7
OlesyaFedorovskaya <nyamka13@mail.ru>
2022-11-24 09:02:24 +0300

    add Task23

--- Task22/Program.cs
+++ Task22/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 22: Напишите программу, которая
+// принимает на вход число (N) и 
+// выдаёт таблицу квадратов чисел от 1 до N.
+// 5 -> 1, 4, 9, 16, 25.
+// 2 -> 1,4
+
+int n = -1;
+while (n < 1)
+{
+    Console.WriteLine("Введите число: ");
+    n = Convert.ToInt32(Console.ReadLine());
+    if (n < 1) Console.WriteLine("Введены неверные данные");
+}
+
+void Quarter(int num)
+{
+    int count = 1;
+    while (count <= num)
+    {
+        Console.WriteLine($"{count}  |  {count * count}");
+        count++;
+    }
+}
+
+Quarter (n);
--- Task23/Program.cs
+++ Task23/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 23. Напишите программу, которая 
+// принимает на вход число (N) и 
+// выдаёт таблицу кубов чисел от 1 до N.
+// 5 ->
+// 1 | 1
+// 2 | 8
+// 3 | 27
+// 4 | 64
+// 5 | 125
+
+Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+void Cube()
+{
+    int count = 1;
+    while (count <= n)
+    {
+        Console.WriteLine($" {count} | {count * count * count}");
+        count++;
+    }
+}
+Cube();
