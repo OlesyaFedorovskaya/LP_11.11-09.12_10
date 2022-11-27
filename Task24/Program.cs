15d1b999fc89a47176d31db374ddefa8692a2307
OlesyaFedorovskaya <nyamka13@mail.ru>
2022-11-27 04:37:19 +0300

    update Task24

--- Task24/Program.cs
+++ Task24/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 24: Напишите программу, которая
+// принимает на вход число (А) и 
+// выдаёт сумму чисел от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int SumDigits(int num)
+{
+    int sum = 0;
+    for (int i = 1; i <= num; i++)
+    {
+        sum = sum + i;
+    }
+
+    return sum;
+}
+
+if (number > 0)
+{
+    int sumDigits = SumDigits(number);
+    Console.WriteLine($"Сумма чисел от 1 до {number} = {sumDigits}");
+}
+else System.Console.WriteLine("Требуется ввести натуральное число");
