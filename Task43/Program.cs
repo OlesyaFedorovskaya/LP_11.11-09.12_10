b43c01850219c8e3867a3fd8e11e84e959f3737d
OlesyaFedorovskaya <nyamka13@mail.ru>
2022-12-06 01:55:19 +0300

    update Task43

--- Task43/Program.cs
+++ Task43/Program.cs
@@ -5,3 +5,44 @@
 
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 
+Console.WriteLine("Введите значение b1: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите значение k1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите значение b2: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите значение k2: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+
+
+double X(double b1, double k1, double b2, double k2)
+{
+    double x = (-b1 + b2) / (k1 - k2);
+    return x;
+}
+
+double Y(double b1, double k1)
+{
+    double x = X(b1, k1, b2, k2);
+    double y = k1 * x + b1;
+    return x;
+}
+
+double x = X(b1, k1, b2, k2);
+double y = Y(b1, k1);
+x = Math.Round(x, 2);
+y = Math.Round(y, 2);
+Console.WriteLine($"Точка пересечения двух прямых равна ({x},{y})");
+
+
+
+// Даны две различные прямые y = k1*x + b1 и y = k2*x + b2. 
+// Найти точку пересечения данных прямых.
+
+// приравниваем правые части данных равенств:
+// k1*x+b1=k2*x+b2
+// выразим отсюда x:
+// x=(b2-b1)/(k1-k2)
+// затем подставим полученное значение х в выражение для у (для любого, например первого):
+// y=k1(b2-b1)/(k1-k2)+b1
+// ответ: x=(b2-b1)/(k1-k2) , y=k1(b2-b1)/(k1-k2)+b1
