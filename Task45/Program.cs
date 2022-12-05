a95b1e7921aac51dbcd05f914efa9336526d3a78
OlesyaFedorovskaya <nyamka13@mail.ru>
2022-12-05 04:19:47 +0300

    update Task44, Task45

--- Task45/Program.cs
+++ Task45/Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 45: Напишите программу, которая 
+// будет создавать копию заданного массива 
+// с помощью поэлементного копирования.
