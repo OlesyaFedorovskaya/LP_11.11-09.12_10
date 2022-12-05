75f0cf7c02c998d7fc5c6b25f0665fcef7f89da4
OlesyaFedorovskaya <nyamka13@mail.ru>
2022-12-05 04:53:24 +0300

    update Task44, Task45

--- Task44/Program.cs
+++ Task44/Program.cs
@@ -3,3 +3,32 @@
 // Если N = 5 -> 0 1 1 2 3
 // Если N = 3 -> 0 1 1
 // Если N = 7 -> 0 1 1 2 3 5 8
+
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int[] Fibonacci(int num)
+{
+    int[] arr = new int[num];
+    arr[1] = 1;
+    for (int i = 2; i < num; i++)
+    {
+        arr[i] = arr[i - 1] + arr[i - 2];
+    }
+    return arr;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+int[] resultArray = Fibonacci(number);
+Console.WriteLine($"Первые {number} чисел Фибоначчи:");
+PrintArray(resultArray);
