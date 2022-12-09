bff57530dd38cd62fcce39ebae5f0949d2ca2226
OlesyaFedorovskaya <nyamka13@mail.ru>
2022-12-09 07:23:36 +0300

    update Task50

--- Task50/Program.cs
+++ Task50/Program.cs
@@ -1,2 +1,59 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 50. Напишите программу, которая 
+// на вход принимает позиции элемента в двумерном массиве, 
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+
+// 1 | 4 | 7 | 2
+//---------------
+// 5 | 9 | 2 | 3
+//---------------
+// 8 | 4 | 2 | 4
+
+// 1, 7 -> такого элемента в массиве нет
+
+Console.Write("Введите номер строки: ");
+int r = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите номер столбца: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns]; 
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++) 
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++) 
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j], 4}, ");
+            else Console.Write($"{matrix[i, j], 4}");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+void ElementSearch(int[,] matrix)
+{
+    if (r <= matrix.GetLength(0) && c <= matrix.GetLength(1))
+        Console.WriteLine($"Значение элемента -> {matrix[r, c]}");
+    else 
+        Console.WriteLine($"Элемента с заданными параметрами в массиве нет.");
+}
+
+int[,] array2D = CreateMatrixRndInt(3, 4, 1, 9);
+PrintMatrix(array2D);
+ElementSearch(array2D);
--- Task52/Program.cs
+++ Task52/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 52. Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+
+// 1 | 4 | 7 | 2
+//---------------
+// 5 | 9 | 2 | 3
+//---------------
+// 8 | 4 | 2 | 4
+
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];  // 0,1
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++) //строки
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++) // столбцы
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j], 4}, ");
+            else Console.Write($"{matrix[i, j], 4}");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+int[,] array2D = CreateMatrixRndInt(3, 4, 1, 9);
+PrintMatrix(array2D);
