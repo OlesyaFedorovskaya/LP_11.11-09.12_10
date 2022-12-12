6b761167c312012d6eb891ab92a42131830f0c30
OlesyaFedorovskaya <nyamka13@mail.ru>
2022-12-13 00:09:31 +0300

    update Task 53

--- Task55/Program.cs
+++ Task55/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 55: Задайте двумерный массив. 
+// Напишите программу, которая заменяет строки на столбцы. 
+// В случае, если это невозможно, программа должна 
+// вывести сообщение для пользователя.
+
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];  // 0,1
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++) //строки
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++) // столбцы
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j], 4}, ");
+            else Console.Write($"{matrix[i, j], 4}");
+        }
+        Console.WriteLine("|");
+    }
+}
+
