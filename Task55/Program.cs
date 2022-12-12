6bb6ff560e16d26daf3279a7c31ab7f0789010b7
OlesyaFedorovskaya <nyamka13@mail.ru>
2022-12-13 01:16:57 +0300

    update Task 55

--- Task55/Program.cs
+++ Task55/Program.cs
@@ -26,10 +26,64 @@ void PrintMatrix(int[,] matrix)
         Console.Write("|");
         for (int j = 0; j < matrix.GetLength(1); j++)
         {
-            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j], 4}, ");
-            else Console.Write($"{matrix[i, j], 4}");
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],4}, ");
+            else Console.Write($"{matrix[i, j],4}");
         }
         Console.WriteLine("|");
     }
 }
 
+int[,] CreateModifiedMatrix(int[,] matrix)
+{
+    int[,] matrix2 = new int[matrix.GetLength(0), matrix.GetLength(1)];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix2[i, j] = matrix[j, i];
+        }
+    }
+    return matrix2;
+}
+
+int[,] matrix = CreateMatrixRndInt(4, 4, 0, 5);
+PrintMatrix(matrix);
+Console.WriteLine();
+if (matrix.GetLength(0) != matrix.GetLength(1))
+{
+    Console.WriteLine("Замена невозможна.");
+}
+else
+{
+    int[,] matrix2 = CreateModifiedMatrix(matrix);
+    PrintMatrix(matrix2);
+}
+
+// bool CheckSquareMatrix(int[,] matrix)
+// {
+//     return matrix.GetLength(0) == matrix.GetLength(1);
+// }
+
+// void ReplaceRowsColumnsMatrix(int[,] matrix)
+// {
+//     int rows = matrix.GetLength(0);
+//     int columns = matrix.GetLength(1);
+//     for (int i = 0; i < rows - 1; i++)
+//     {
+//         for (int j = i + 1; j < columns; j++)
+//         {
+//             int temp = matrix[i, j];
+//             matrix[i, j] = matrix[j, i];
+//             matrix[j, i] = temp;
+//         }
+//     }
+// }
+
+// int[,] array2D = CreateMatrixRndInt(4, 4, 0, 5);
+// PrintMatrix(array2D);
+// Console.WriteLine();
+// ReplaceRowsColumnsMatrix(array2D);
+// if (CheckSquareMatrix(array2D) == false)
+//     Console.WriteLine("Замена невозможна.");
+// else
+//     PrintMatrix(array2D);
