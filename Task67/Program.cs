474c44b14c61d9fe14ca9fe3afa9764740523856
OlesyaFedorovskaya <nyamka13@mail.ru>
2022-12-16 01:01:07 +0300

    update Task67

--- Task67/Program.cs
+++ Task67/Program.cs
@@ -3,3 +3,13 @@
 // возвращать сумму его цифр.
 // 453 -> 12
 // 45 -> 9
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int SumDigits(int numb)
+{
+    return numb != 0 ? numb % 10 + SumDigits(numb / 10) : 0;
+}
+
+Console.WriteLine(SumDigits(Math.Abs(number)));
