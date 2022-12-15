8ffa0a450efb2abbb2cfc7bff9d9169349385b09
OlesyaFedorovskaya <nyamka13@mail.ru>
2022-12-15 15:28:08 +0300

    added Task63-Task69

--- Task58/Program.cs
+++ Task58/Program.cs
@@ -8,3 +8,49 @@
 // 18 20
 // 15 18
 
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];  
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++) 
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++) 
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+int[,] CreateMatrixRndInt2(int rows, int columns, int min, int max)
+{
+    int[,] matrix2 = new int[rows, columns];  
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix2.GetLength(0); i++) 
+    {
+        for (int j = 0; j < matrix2.GetLength(1); j++) 
+        {
+            matrix2[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix2;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j], 4}, ");
+            else Console.Write($"{matrix[i, j], 4}");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+int[,] array2D = CreateMatrixRndInt(4, 4, 1, 9);
+PrintMatrix(array2D);
---
+++ Task65/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 65: Задайте значения M и N. 
+// Напишите программу, которая
+// выведет все натуральные числа в промежутке от M до N.
+
+Console.WriteLine("Введите первое натуральное число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе натуральное число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+NaturalNumbers(number1, number2);
+
+void NaturalNumbers(int num1, int num2)
+{
+    if (num1 > num2) 
+    {
+        //Console.Write($"{num2} "); // от 6 до 1
+        NaturalNumbers(num1, num2 + 1);
+        Console.Write($"{num2} "); // от 1 до 6
+    }
+    if (num1 < num2) 
+    {
+        Console.Write($"{num1} "); // от 6 до 1
+        NaturalNumbers(num1 + 1, num2);
+        //Console.Write($"{num1} "); // от 1 до 6
+    }
+    if (num1 == num2) 
+        Console.Write($"{num1} "); // от 6 до 1
+      //Console.Write($"{num} "); // от 1 до 6
+    
+}
